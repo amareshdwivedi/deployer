f613769589b42d536b7ea6991bb6c894223362ad
adwivedi <amaresh.dwivedi@nutanix.com>
2015-04-20 12:54:07 +0530

    initial commit

---
+++ src/main/java/com/nutanix/deployer/App.java
@@ -0,0 +1,13 @@
+package com.nutanix.deployer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
